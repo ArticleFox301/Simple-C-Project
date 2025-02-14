3c87432aa707b5e34cd88a7855d074022fb5cc74
ArticleFox301 <duc21112003@gmail.com>
2025-02-14 09:07:52 +0700

    Add number guessing project

---
+++ numberGuessing.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    srand(static_cast<unsigned int>(time(0)));
+
+    int secretNumber = rand() % 100 + 1;
+    int guess=0;
+
+    cout << "Chose number between 1 and 100"<< endl;
+
+    while(guess != secretNumber){
+        cout << "Enter your guess: ";
+        cin >> guess;
+        if(guess > secretNumber){
+            cout << "Too high! Try again"<< endl;
+        } else if (guess < secretNumber){
+            cout << "Too Low! Try again"<< endl;
+        } else {
+            cout << "Congratulations! You won"<< endl;
+        }
+    }
+    return 0;
+}
