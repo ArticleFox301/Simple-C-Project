b1db52596556757b8406bb487cf4b19901d36f64
ArticleFox301 <duc21112003@gmail.com>
2025-02-18 08:20:30 +0700

    Add simple tic_tac_toe game

---
+++ TicTacToeGame.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void drawBoard(const vector<vector<char>> &board){
+    for(const auto &row : board){
+        for(char cell : row){
+            cout << cell << " ";
+        }
+        cout << endl;
+    }
+}
+
+bool checkWin(const vector<vector<char>> &board, char player){
+    for(int i = 0; i<3; ++i){
+        if((board[i][0] == player && board[i][1] == player && board[i][2] == player) || 
+            (board[0][i] == player && board[1][i] == player && board[2][i] == player)){
+                return true;
+        }
+    }
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
+        return true;
+    }
+    return false;
+}
+
+bool checkDraw(const vector<vector<char>> &board){
+    for(const auto &row : board){
+        for(char cell : row){
+            if(cell = ' '){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main(){
+    std::vector<std::vector<char>> board(3, std::vector<char>(3, ' '));
+    char currentPlayer = 'X';
+    int row, col;
+    bool gameOver = false;
+
+    while (!gameOver){
+        drawBoard(board);
+        std::cout << "Player " << currentPlayer << ", enter row and column (0-2): ";
+        std::cin >> row >> col;
+        if (board[row][col] == ' '){
+            board[row][col] = currentPlayer;
+            gameOver = checkWin(board, currentPlayer);
+
+            if (gameOver){
+                drawBoard(board);
+                std::cout << "Player " << currentPlayer << " wins!" << std::endl;
+            } else if (checkDraw(board)){
+              drawBoard(board);
+              std::cout << "It's a draw!" << std::endl;
+              gameOver = true;
+            }
+
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        } else{
+            std::cout << "That spot is already taken. Try again." << std::endl;
+        }
+    }
+
+    return 0;
+}
